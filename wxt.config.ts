9241454938e4d7d17cb4c6eddde059fb0da90e83
burak unver <burakhanunver@gmail.com>
2025-11-09 14:19:28 +0300

    add manifest name and permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,12 @@ import tailwindcss from "@tailwindcss/vite"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest: {
+    name: "JavaScript Blocker",
+    description: "Block JavaScript on websites",
+    permissions: ["storage", "declarativeNetRequest", "declarativeNetRequestFeedback"],
+    host_permissions: ["<all_urls>"],
+  },
   srcDir: "src",
   modules: ["@wxt-dev/module-svelte"],
   vite: () => ({
