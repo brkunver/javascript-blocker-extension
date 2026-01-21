eced1e443d6f85874922afe6c2ea575eb96b5594
burak unver <burakhanunver@gmail.com>
2026-01-21 17:00:05 +0300

    add i18n support

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite"
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    name: "__MSG_EXTENSION_NAME__",
-    description: "__MSG_EXTENSION_DESCRIPTION__",
+    name: "__MSG_extensionName__",
+    description: "__MSG_extensionDescription__",
     permissions: ["storage", "declarativeNetRequest", "declarativeNetRequestFeedback", "webNavigation"],
     host_permissions: ["<all_urls>"],
     default_locale: "en",
