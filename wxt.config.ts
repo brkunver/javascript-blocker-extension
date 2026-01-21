bac80c6c4c2d70c9f12e45e08e068dacd84919b7
burak unver <burakhanunver@gmail.com>
2026-01-21 16:53:16 +0300

    add extension name

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite"
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    name: "JavaScript Blocker",
-    description: "Block JavaScript on websites",
+    name: "__MSG_EXTENSION_NAME__",
+    description: "__MSG_EXTENSION_DESCRIPTION__",
     permissions: ["storage", "declarativeNetRequest", "declarativeNetRequestFeedback", "webNavigation"],
     host_permissions: ["<all_urls>"],
     default_locale: "en",
